0b5519a78f3670afa116e016f94d1dc16d6e38b2
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-07-23 21:04:07 +0300

    Implement animations.

--- Search/Controllers/DimmingPresentationController.swift
+++ Search/Controllers/DimmingPresentationController.swift
@@ -14,4 +14,32 @@ class DimmingPresentationController: UIPresentationController {
     override var shouldRemovePresentersView: Bool {
         return false
     }
+
+    // MARK: GUI variables
+
+    lazy var dimmingView = GradientView(frame: CGRect.zero)
+
+    // MARK: Methods
+
+    override func presentationTransitionWillBegin() {
+        dimmingView.frame = containerView!.bounds
+        containerView!.insertSubview(dimmingView, at: 0)
+
+        dimmingView.alpha = 0
+        if let coordinator =
+            presentedViewController.transitionCoordinator {
+            coordinator.animate(alongsideTransition: { _ in
+                self.dimmingView.alpha = 1
+            }, completion: nil)
+        }
+    }
+
+    override func dismissalTransitionWillBegin()  {
+        if let coordinator =
+            presentedViewController.transitionCoordinator {
+            coordinator.animate(alongsideTransition: { _ in
+                self.dimmingView.alpha = 0
+            }, completion: nil)
+        }
+    }
 }
