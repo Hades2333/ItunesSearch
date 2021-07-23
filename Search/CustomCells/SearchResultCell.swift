59598a51454ec58f0d31031c99ffd281803e723c
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-07-23 18:46:03 +0300

    Implement URLSession async image download

--- Search/CustomCells/SearchResultCell.swift
+++ Search/CustomCells/SearchResultCell.swift
@@ -15,6 +15,12 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
 
+    // MARK: Properties
+
+    var downloadTask: URLSessionDownloadTask?
+
+    // MARK: Lifecycle
+
     override func awakeFromNib() {
         super.awakeFromNib()
 
@@ -22,6 +28,27 @@ class SearchResultCell: UITableViewCell {
         selectedView.backgroundColor = UIColor(red: 20/255,
                                                green: 160/255, blue: 160/255, alpha: 0.5)
         selectedBackgroundView = selectedView
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        downloadTask?.cancel()
+        downloadTask = nil
+    }
+
+    // MARK:- Public Methods
+
+    func configure(for result: SearchResult) {
+        nameLabel.text = result.name
+        if result.artist.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", result.artist, result.type)
+        }
+        artworkImageView.image = UIImage(named: "Placeholder")
+        if let smallURL = URL(string: result.imageSmall) {
+          downloadTask = artworkImageView.loadImage(url: smallURL)
+        }
     }
 
 }
